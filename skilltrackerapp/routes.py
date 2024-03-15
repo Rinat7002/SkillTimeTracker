6970319337ca98293c8287ba62c62f07d5ad2e03
Rinat7002 <rinat_idrisov_2016@mail.ru>
2024-03-15 21:37:11 +0500

    worked version (v1.0.0) with flask

--- skilltrackerapp/routes.py
+++ skilltrackerapp/routes.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 # Подключаем объект приложения Flask из __init__.py
-from skilltrackerapp import app, db
+from skilltrackerapp import app #,db
 # Подключаем библиотеку для "рендеринга" html-шаблонов из папки templates
 from flask import render_template, make_response, request, Response, jsonify, json
 
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('skills-page.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
